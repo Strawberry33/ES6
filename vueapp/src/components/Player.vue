cb1385bb185b59aeb6600fa44d0c996f9f244170
Strawberry33 <949906055@qq.com>
2018-08-16 18:34:56 +0800

    ES6

--- vueapp/src/components/Player.vue
+++ vueapp/src/components/Player.vue
@@ -32,12 +32,18 @@
       <div class="audio">
            <audio ref="musicAudio" @play="isPlay=true" @pause="isPlay=false" class="audio-ctrl" :src="musicSrc" autoplay controls></audio>
       </div>
+      <ul class="lrclist"  ref="lrclist">
+          <li :class=" lrcIndex==index?'selected':'' " v-for="(lrc,index) in lrcList" :key="lrc.time" >
+              {{lrc.lrc}}
+          </li>
+      </ul>
      </div>
 </template>
 
 
 <script>
 import "@/assets/font/iconfont.css";
+import axios from 'axios';
 export default{
    props:["musicList"],
    data(){
@@ -49,7 +55,8 @@ export default{
             isPlay:false,
             toggleList:true,
             musicSrc:"",
-            
+            lrcList:[],//歌词的数组
+            lrcIndex:-1,  
        };
    },
    methods: {
@@ -57,7 +64,10 @@ export default{
       this.nowIndex = index;
     },
      play() {
-      this.$refs.musicAudio.play();
+         if(this.nowIndex != -1){
+             this.$refs.musicAudio.play();
+             }
+     
     },
     pause() {
       this.$refs.musicAudio.pause();
@@ -74,6 +84,33 @@ export default{
         this.nowIndex = 0;
       }
     },
+    //解析歌词
+    parseLrc(text){
+        // console.log(text);
+        //按照行分隔
+        let line=text.split('\n');
+        // console.log(line);
+        //时间 歌词 分隔
+        line.forEach(elem=>{
+            // reg.exec(str);正则表达式下面的方法
+            // str.match(reg);字符下的reg正则表达式
+            let time=elem.match(/\[\d{2}:\d{2}.\d{2}\]/);
+            if (time!=null){
+                let lrc=elem.split(time)[1];
+                // console.log(time[0],lrc);
+                let timeReg=time[0].match(/(\d{2}):(\d{2}).(\d{2})/);
+                let time2Seconds=parseInt(timeReg[1])*60+parseInt(timeReg[2])+parseInt(timeReg[3])/1000;
+                // console.log(time2Seconds,lrc);
+                // this.lrcList.push([time2Seconds,lrc]);
+                this.lrcList.push(
+                    {
+                        time:time2Seconds,
+                        lrc:lrc,
+                    }
+                );
+            } 
+        });
+    },
    },
 watch: {
     nowIndex() {
@@ -82,7 +119,33 @@ watch: {
       this.albumTitle = nowMusic.title;
       this.albumAuthor = nowMusic.author;
       this.musicSrc = nowMusic.src;
+      this.lrcList=[];
+      this.lrcIndex=-1;
+    //   console.log(nowMusic.lrc); 默认的路径就是lrc
+      axios.get('/' + nowMusic.lrc).then(res=>{
+        //    console.log(res.data);
+        this.parseLrc(res.data);
+           //输出之后需要的是.data,所以res.data
+      });
     }
+  },
+  mounted(){
+      let musicAudio= this.$refs.musicAudio;
+      this.$refs.musicAudio.addEventListener(
+          'timeupdate',()=>{
+            //   console.log(musicAudio.currentTime);
+            let currentTime= musicAudio.currentTime;
+            // console.log(this.lrcList[0]);
+            // console.log(currentTime);
+            this.lrcList.forEach((elem,index)=>{
+                console.log(elem.time,currentTime);
+                if(Math.ceil(elem.time) >= currentTime &&  Math.floor(elem.time) < currentTime){
+                     this.lrcIndex = index;
+                      this.$refs.lrclist.scrollTop = this.lrcIndex * 25;
+                       }
+            });
+          }
+      );//括号里绑定事件类型，然后箭头函数,timeupdate时间改变被触发的事件
   }
    
 }
@@ -98,13 +161,13 @@ watch: {
 .music-list{
     position:fixed;
     bottom:1.8rem;
-    background-color: #2a2929;
+    background-color:#fff;
     width:100%;
     height:4rem;
     overflow-y:scroll;
     &-item{
-        color:#fff;
-        border-bottom: 1px solid #343433;
+        color:rgb(199, 123, 199);
+        border-bottom: 1px solid #fff;
         padding:0.1rem;
 
         &.selected{
@@ -182,4 +245,20 @@ watch: {
   }
 }
 
+.lrclist{
+  text-align: center;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 2rem;
+  top: 3.3rem;
+  overflow-y: scroll;
+  z-index: -1;
+  padding-top: 2rem;
+
+  .selected{
+    color: #299557;
+    font-size: 150%;
+  }
+}
 </style>
