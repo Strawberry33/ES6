ba32eba6ee28f747b721ad5e5b9f3de4cc7b5fab
Strawberry33 <949906055@qq.com>
2018-10-19 23:03:52 +0800

    ES6

--- vueapp/src/views/total/Total.vue
+++ vueapp/src/views/total/Total.vue
@@ -2,7 +2,7 @@
 <div>
 <div class='top'>
 <p class='a'> &lt;</p >
-<p class='title'>农机租赁</p >
+<p class='title'>租赁确认页面</p >
 </div>
 <hr>
 <div class='middle'>
@@ -27,7 +27,25 @@
 <div class="line">租赁日期：<span class="line-text">10月3号星期三12:00——10月9号星期五12:00</span></div><hr>
 </div>
 <div class='bottom'>
-<button class='save'  @click="isLogin=true">确认订单</button>
+<button class='save' @click="isLogin=true">确认订单</button>
+</div>
+<div id='mask'>
+    <div id='pay'>
+    <div class='payone'>
+    <p class='payone-a'>X</p> <p class='payone-b'>确认付款</p> <p class='payone-c'>？</p>
+    </div>
+    <div class='paytwo'>
+    ￥29.00
+    </div>
+    <div class='paythree'><p class='paythree-a'>支付宝账号</p> <p class='paythree-b'>188******67</p></div>
+    <div class='payfour'>
+    <p class='payfour-a'>付款方式</p>  <p class='payfour-b'>支付宝 &lt;</p>
+    </div>
+    <div class='payfive'>
+    <p class='payfive-a'>立&nbsp;即&nbsp;付&nbsp;款</p>
+    </div>
+    <div></div>
+    </div>
 </div>
 </div>
 </template>
@@ -100,6 +118,7 @@ color: #666;
 }
 .bottom{
 position:relative;
+margin:0;
 }
 .save{
 width: 2rem;
@@ -112,6 +131,85 @@ line-height: 0.6rem;
 position: absolute;
 right: 0.2rem;
 border: 0;
+}
+#mask{
+    position:absolute;
+    top:0;
+    bottom:0;
+    left:0;
+    right:0;
+    background:rgba(0,0,0, .6);
+    height:100%;
+}
+#pay{
+    position:absolute;
+    width: 100%;
+    height: 60%;
+    background: white;
+    color: black;
+    bottom:0px;
+        }
+.payone{
+    display:flex;
+    flex-direction:row;
+    height:0.7rem;
+}
+.payone-a{
+    flex-grow:1;
+    margin-left:0.9rem;
+    margin-top:0.1rem;
+}
+.payone-b{
+    flex-grow:1;
+    margin-top:0.1rem;
+}
+.payone-c{
+    flex-grow:1;
+    margin-right:-1rem;
+    margin-top:0.1rem;
+}
+.paytwo{
+    text-align:center;
+    margin-top:0.2rem;
+    font-size:0.5rem;
+}
+.paythree{
+    margin-top:0.3rem;
+    overflow:hidden;
+}
+.paythree-a{
+    float:left;
+    margin-left:0.3rem;
+}
+.paythree-b{
+    float:right;
+     margin-right:0.3rem;
+}
+.payfour{
+    margin-top:0.3rem;
+    overflow:hidden;
+}
+.payfour-a{
+    float:left;
+     margin-left:0.3rem;
+}
+.payfour-b{
+    float:right;
+    margin-right:0.3rem;
+}
+.payfive{
+    position:absolute;
+    background-color:rgb(100,200,230);
+    width:96%;
+    height:10%;
+    border-radius: 0.2rem;
+    margin-left:2%;
+    bottom:0px;
+    text-align:center;
+    color:white;
+    &-a{
+        font-size:0.4rem;
+    }
 }
 
 </style>
